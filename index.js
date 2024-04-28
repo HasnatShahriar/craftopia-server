3674b061c5cfda98c1d78b1522407a0cf7ef4397
HasnatShahriar <shahriarhriday94@gmail.com>
2024-04-29 00:03:54 +0600

    add update & delete routing

--- index.js
+++ index.js
@@ -9,12 +9,9 @@ const port = process.env.PORT || 5000
 app.use(cors());
 app.use(express.json());
 
-// craftopia
-// 9vHViDW9xrQVEaCw
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ihpbk8d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -41,20 +38,26 @@ async function run() {
     })
 
 
+    app.get('/crafts/:id',async(req,res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await craftCollection.findOne(query);
+      res.send(result);
+    })
 
-    app.get('/crafts/:id', async (req, res) => {
-      const craftId = req.params.id;
-      try {
-        const craft = await craftCollection.findOne({ _id: new ObjectId(craftId) });
-        if (craft) {
-          res.send(craft);
-        } else {
-          res.status(404).send({ error: 'Craft not found' });
-        }
-      } catch (error) {
-        res.status(500).send({ error: 'Internal server error' });
-      }
-    });
+
+
+    app.get('/myList/:email',async(req,res)=>{
+      console.log(req.params.email);
+      const result = await craftCollection.find({email: req.params.email}).toArray();
+      res.send(result);
+    })
+
+
+    app.get('/singleProduct/:id',async(req,res)=>{
+      const result = await craftCollection.findOne({_id:new ObjectId (req.params.id)})
+      res.send(result)
+    })
 
 
     app.post('/crafts',async(req,res)=>{
@@ -64,18 +67,37 @@ async function run() {
       res.send(result);
     })
 
-    app.get('/myList/:email',async(req,res)=>{
-      console.log(req.params.email);
-      const result = await craftCollection.find({email: req.params.email}).toArray();
+
+    app.put('/updateProduct/:id', async (req,res)=>{
+      const id = req.params.id
+      const filter = {_id: new ObjectId(id)};
+      const options = {upsert : true};
+      const updatedProduct = req.body;
+      const product = {
+        $set:{
+          photo: updatedProduct.photo,
+          item: updatedProduct.item,
+          subCategory: updatedProduct.subCategory,
+          description: updatedProduct.description,
+          price: updatedProduct.price,
+          rating: updatedProduct.rating,
+          customization: updatedProduct.customization,
+          processing: updatedProduct.processing,
+          stock: updatedProduct.stock,
+          email: updatedProduct.email,
+          name: updatedProduct.name,              
+        }
+      }
+
+      const result = await craftCollection.updateOne(filter,product,options);
       res.send(result);
     })
+    
 
-    // user related api
-    app.post('/users',async(req,res)=>{
-      const user = req.body;
-      console.log(user);
-      const result = await userCollection.insertOne(user);
-      res.send(result);
+    app.delete('/delete/:id',async(req,res)=>{
+      const result = await craftCollection.deleteOne({_id : new ObjectId(req.params.id)})
+      console.log(result);
+      res.send(result)
     })
 
 
